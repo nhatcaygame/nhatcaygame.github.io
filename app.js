152c2c91b67a43a38c81310cc4203594ca6864ab
Vu Minh Nhat <88591831+nhatcaygame@users.noreply.github.com>
2022-05-02 23:07:26 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const yourDate = new Date("2022-01-27T23:30:01");
+document.addEventListener('DOMContentLoaded', () => {
+    var rootTime = document.querySelector('time');
+    document.querySelector('ani').textContent = `${(yourDate.getDate() >9)?yourDate.getDate():"0"+yourDate.getDate()}-${(yourDate.getDate()>8) ?(yourDate.getMonth()+1):"0" + (yourDate.getMonth()+1)}-${(yourDate.getFullYear())}`;
+    document.querySelector('date').textContent = Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) + "DAYS";
+
+    function oclock() {
+        var today = new Date(),
+        hrs = (Math.floor(
+            Math.floor((today - yourDate) / 1000) / 60
+        )) % 24,
+        min = (Math.floor(Math.floor((today - yourDate) / 1000) / 60
+        )) % 60,
+        sec = Math.floor((today - yourDate) / 1000) % 60;
+
+        rootTime.textContent = `${(hrs> 9)? hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
+    } oclock();
+    var timer = setInterval(() => {
+        oclock();
+    },1000);
+    document.getElementsByTagName('body')[0].insertAdjacentHTML("beforeend","<div id='mask'></div>");
+},false);
